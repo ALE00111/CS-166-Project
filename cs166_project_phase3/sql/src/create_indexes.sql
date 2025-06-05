a35202bdac4ed18a6956719ae51a65c35ea87ae0
ALE00111 <austintle56@gmail.com>
2025-06-05 14:36:38 -0700

    Fixing index file

--- cs166_project_phase3/sql/src/create_indexes.sql
+++ cs166_project_phase3/sql/src/create_indexes.sql
@@ -2,10 +2,8 @@ DROP INDEX IF EXISTS FlightNum_index;
 DROP INDEX IF EXISTS PlaneID_index;
 DROP INDEX IF EXISTS TechnicianID_index;
 DROP INDEX IF EXISTS ReservationID_index;
-DROP INDEX IF EXISTS ;
 
 CREATE INDEX FlightNum_index on FlightInstance(FlightNumber);
 CREATE INDEX PlaneID_index on MaintenanceRequest(PlaneID);
 CREATE INDEX TechnicianID_index on Repairs(TechnicianID);
 CREATE INDEX ReservationID_index on Reservation(ReservationID);
-CREATE INDEX 
